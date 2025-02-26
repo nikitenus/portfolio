bf6d8596bfb2817d1e96d0d8bd2b480de86c79ad
nikitenus <nikitenuss@mail.ru>
2025-02-26 20:49:08 +0300

    feat: add contact section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Main} from './layout/sections/main/Main.tsx';
 import {Skills} from './layout/sections/skills/Skills.tsx';
 import {Works} from './layout/sections/works/Works.tsx';
 import {Testimony} from './layout/sections/testimony/Testimony.tsx';
+import {Contact} from './layout/sections/contacts/Contact.tsx';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Skills/>
             <Works/>
             <Testimony/>
+            <Contact/>
         </div>
     )
 }
---
+++ src/layout/sections/contacts/Contact.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import {SectionTitle} from '../../../components/SectionTitle.tsx';
+import {Button} from '../../../components/Button.tsx';
+
+export const Contact = () => {
+    return (
+        <StyledContacts>
+            <SectionTitle>Contact</SectionTitle>
+            <StyledForm>
+                <Field placeholder={'name'}/>
+                <Field placeholder={'subject'}/>
+                <Field placeholder={'message'} as={'textarea'}/>
+                <Button type={'submit'}>Send message</Button>
+            </StyledForm>
+        </StyledContacts>
+    );
+};
+
+const StyledContacts = styled.section`
+    min-height: 50vh;
+    background-color: #ef997b;
+`
+
+const StyledForm = styled.form`
+    max-width: 500px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin: 0 auto;
+`
+
+const Field = styled.input`
+
+`
