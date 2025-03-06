303fb46b7cf59180165afa082e98aa0b2ee1bfd5
nikitenus <nikitenuss@mail.ru>
2025-03-06 11:20:47 +0300

    feat: create style for main section

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,24 +1,30 @@
 import styled from 'styled-components';
 import {Logo} from '../../components/logo/Logo.tsx';
-import {Menu} from '../../components/menu/Menu.tsx';
 import {Container} from '../../components/Container.ts';
 import {FlexWrapper} from '../../components/FlexWrapper.tsx';
+import {HeaderMenu} from './headerMenu/HeaderMenu.tsx';
 
 const items = ['Home', 'Skills', 'Works', 'Testimony', 'Contact']
 
 export const Header = () => {
     return (
-        <StyledHeader>
+        <StyledHeaderMenu>
             <Container>
-                <FlexWrapper justify={"space-between"} align={"center"}>
+                <FlexWrapper justify={'space-between'} align={'center'}>
                     <Logo/>
-                    <Menu menuItems={items}/>
+                    <HeaderMenu menuItems={items}/>
                 </FlexWrapper>
             </Container>
-        </StyledHeader>
+        </StyledHeaderMenu>
     );
 };
 
-const StyledHeader = styled.header`
-    background-color: #d4ffd3;
+const StyledHeaderMenu = styled.nav`
+    background: rgba(31, 31, 32, 0.9);
+    padding: 20px 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 99999;
 `
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,18 +1,27 @@
 import styled from 'styled-components';
 import photo from '../../../assets/images/photo.jpeg'
 import {FlexWrapper} from '../../../components/FlexWrapper.tsx';
+import {Container} from '../../../components/Container.ts';
+import {theme} from '../../../styles/Theme.ts';
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={'center'} justify={'space-around'}>
-                <div>
-                    <span>Hi There</span>
-                    <Name>I am Vladislav Nikitenkov</Name>
-                    <MainTitle>A Web Developer.</MainTitle>
-                </div>
-                <Photo src={photo} alt=""/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper align={'center'} justify={'space-between'}>
+                    <div>
+                        <SmallText>Hi There</SmallText>
+                        <Name>
+                            I am
+                            <span>Vladislav Nikitenkov</span>
+                        </Name>
+                        <MainTitle>A Web Developer.</MainTitle>
+                    </div>
+                    <PhotoWrapper>
+                        <Photo src={photo} alt=""/>
+                    </PhotoWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
@@ -20,6 +29,7 @@ export const Main = () => {
 const StyledMain = styled.section`
     min-height: 100vh;
     background-color: beige;
+    display: flex;
 `
 
 const Photo = styled.img`
@@ -28,10 +38,52 @@ const Photo = styled.img`
     object-fit: cover;
 `
 
-const MainTitle = styled.h1`
+const PhotoWrapper = styled.div`
+    position: relative;
+    z-index: 1;
 
+    &::before {
+        content: "";
+        width: 360px;
+        height: 470px;
+        border: 5px solid ${theme.colors.accent};
+        position: absolute;
+        top: -24px;
+        left: 24px;
+        z-index: -1;
+    }
+`
+
+const MainTitle = styled.h1`
+    font-size: 27px;
+    font-weight: 400;
 `
 
 const Name = styled.h2`
+    font-family: "Josefin Sans", sans-serif;
+    font-size: 50px;
+    font-weight: 700;
+    letter-spacing: 0.05em;
+    margin: 10px 0;
 
+    span {
+        position: relative;
+        z-index: 0;
+
+        &::before {
+            content: "";
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.colors.accent};
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+        }
+    }
+`
+
+const SmallText = styled.h2`
+    font-size: 14px;
+    font-weight: 400;
 `
