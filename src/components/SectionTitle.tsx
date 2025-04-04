944f409515e2427d51c12dfd51e101d1b412a818
Vlad Nikitenkov <nikitenuss@mail.ru>
2025-04-04 10:47:48 +0300

    feat: add responsiveness to Skills section

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 import {theme} from "../styles/Theme.ts";
+import {font} from "../styles/Common.ts";
 
 export const SectionTitle = styled.h2`
+    ${font({family: '"Josefin Sans", sans-serif', weight: 600, Fmin: 30, Fmax: 36})}
+
     text-align: center;
-    font-family: "Josefin Sans", sans-serif;
-    font-size: 36px;
-    font-weight: 600;
     letter-spacing: 5px;
     margin-bottom: 90px;
-    
+
     position: relative;
 
     &::before {
@@ -17,10 +17,14 @@ export const SectionTitle = styled.h2`
         width: 55px;
         height: 1px;
         background-color: ${theme.colors.accent};
-        
+
         position: absolute;
         left: 50%;
         bottom: -30px;
         transform: translateX(-50%);
+
+        @media ${theme.media.mobile} {
+            bottom: -24px;
+        }
     }
 `
