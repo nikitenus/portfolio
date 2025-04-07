3367b49b92380ca5129f822934e64cd1df63e726
Vlad Nikitenkov <nikitenuss@mail.ru>
2025-04-07 19:25:08 +0300

    fix: minor fix

--- src/layout/sections/works/Works_Styles.ts
+++ src/layout/sections/works/Works_Styles.ts
@@ -100,7 +100,6 @@ const TabMenu = styled.nav`
         justify-content: space-between;
         max-width: 352px;
         width: 100%;
-        border: 1px solid red;
         margin: 0 auto 40px;
     }
 `
